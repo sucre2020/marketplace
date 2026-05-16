6ec3f813b790245ae6ea92871cc8b8154e286203
Efita <ettasucre@gmail.com>
2026-05-16 16:49:21 +0100

    in settings, updated file to accepted media upload

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
